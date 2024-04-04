022b12cb0357a4470bfe35fc108766e6eb6cc92b
Andrija <andrijaa.dukic@gmail.com>
2024-04-04 22:11:16 +0200

    improved example code

--- examples/html/websocket.js
+++ examples/html/websocket.js
@@ -1,18 +1,62 @@
 // websocket.js
-const socket = new WebSocket('ws://localhost:8080/telemetry');
 
-socket.addEventListener('message', async (event) => {
-    const data = await event.data.text();
-    const sensorReading = JSON.parse(data);
+const playButton = document.getElementById("play-button");
+let isPlaying = false;
 
-    // Create a new row
-    const newRow = document.createElement('tr');
-    newRow.innerHTML = `
+const table = document.getElementById("data-table");
+
+// Create a global variable to hold the WebSocket connection
+let socket;
+
+playButton.addEventListener("click", () => {
+    if (isPlaying) {
+        // Pause audio (show play icon)
+        playButton.textContent = "▶";
+        // Fade out the table
+        table.classList.remove("fadeIn");
+
+        // Close the websocket connection
+        socket.close()
+    } else {
+        // Play audio (show pause icon)
+        playButton.textContent = "❚❚";
+        // Fade in the table
+        table.classList.add("fadeIn");
+
+        // Initialize the websocket connection
+        initializeWebSocket()
+    }
+    isPlaying = !isPlaying;
+});
+
+function initializeWebSocket() {
+    socket = new WebSocket("ws://localhost:8080/telemetry");
+
+    socket.addEventListener("open", (event) => {
+        console.log("WebSocket connection opened:", event);
+    });
+
+    socket.addEventListener("message", async (event) => {
+        const data = await event.data.text();
+        const sensorReading = JSON.parse(data);
+
+        // Create a new row
+        const newRow = document.createElement("tr");
+        newRow.innerHTML = `
         <td>${sensorReading.sensorId}</td>
         <td>${sensorReading.temperature}</td>
     `;
 
-    // Append the row to the table
-    const tableBody = document.querySelector('#data-table tbody');
-    tableBody.appendChild(newRow);
-});
+        // Append the row to the table
+        const tableBody = document.querySelector("#data-table tbody");
+        tableBody.appendChild(newRow);
+    });
+
+    socket.addEventListener("error", (event) => {
+        console.error("WebSocket error:", event);
+    });
+
+    socket.addEventListener("close", (event) => {
+        console.log("WebSocket connection closed:", event);
+    });
+}
